a14dc4b0581ca1d390adb28b6f7595497dc0222c
Qi Zhao <zhaoqi99@outlook.com>
2023-03-06 10:47:36 +0800

    feat: 🎸 sync latest Django Collector https://github.com/django/django/blob/b05dfc289429dc4de990617da543af681d6638c7/django/db/models/deletion.py

--- django_softdelete_mixin/deletion.py
+++ django_softdelete_mixin/deletion.py
@@ -1,42 +1,84 @@
 from collections import Counter
-from operator import attrgetter
+from functools import reduce
+from operator import attrgetter, or_
 
-from django.db import transaction
-from django.db.models import signals, sql
+from django.db import models, transaction
+from django.db.models import query_utils, signals, sql
 from django.db.models.deletion import Collector
 
 
 class SoftDeleteCollector(Collector):
-    def related_objects(self, related, objs):
-        return related.related_model.objects.using(self.using).filter(
-            **{"%s__in" % related.field.name: objs}
+    def related_objects(self, related_model, related_fields, objs):
+        """
+        Get a QuerySet of the related model to objs via related fields.
+        """
+        predicate = query_utils.Q.create(
+            [(f"{related_field.name}__in", objs) for related_field in related_fields],
+            connector=query_utils.Q.OR,
         )
+        return related_model._base_manager.using(self.using).filter(predicate)
     
     def delete(self):
+        # sort instance collections
         for model, instances in self.data.items():
             self.data[model] = sorted(instances, key=attrgetter("pk"))
+
+        # if possible, bring the models in an order suitable for databases that
+        # don't support transactions or cannot defer constraint checks until the
+        # end of a transaction.
         self.sort()
         # number of objects deleted for each model label
         deleted_counter = Counter()
 
+        # Optimize for the case with a single obj and no dependencies
+        if len(self.data) == 1 and len(instances) == 1:
+            instance = list(instances)[0]
+            if self.can_fast_delete(instance):
+                with transaction.mark_for_rollback_on_error(self.using):
+                    count = sql.DeleteQuery(model).delete_batch(
+                        [instance.pk], self.using
+                    )
+                setattr(instance, model._meta.pk.attname, None)
+                return count, {model._meta.label: count}
+
         with transaction.atomic(using=self.using, savepoint=False):
+            # send pre_delete signals
             for model, obj in self.instances_with_model():
                 if not model._meta.auto_created:
                     signals.pre_delete.send(
-                        sender=model, instance=obj, using=self.using
+                        sender=model,
+                        instance=obj,
+                        using=self.using,
+                        origin=self.origin,
                     )
 
             # fast deletes
             for qs in self.fast_deletes:
-                count = qs.update(is_deleted=True)
-                deleted_counter[qs.model._meta.label] += count
+                count = qs._raw_delete(using=self.using)
+                if count:
+                    deleted_counter[qs.model._meta.label] += count
 
             # update fields
-            for model, instances_for_fieldvalues in self.field_updates.items():
-                for (field, value), instances in instances_for_fieldvalues.items():
+            for (field, value), instances_list in self.field_updates.items():
+                updates = []
+                objs = []
+                for instances in instances_list:
+                    if (
+                        isinstance(instances, models.QuerySet)
+                        and instances._result_cache is None
+                    ):
+                        updates.append(instances)
+                    else:
+                        objs.extend(instances)
+                if updates:
+                    combined_updates = reduce(or_, updates)
+                    combined_updates.update(**{field.name: value})
+                if objs:
+                    model = objs[0].__class__
                     query = sql.UpdateQuery(model)
-                    query.update_batch([obj.pk for obj in instances],
-                                       {field.name: value}, self.using)
+                    query.update_batch(
+                        list({obj.pk for obj in objs}), {field.name: value}, self.using
+                    )
 
             # reverse instance collections
             for instances in self.data.values():
@@ -44,22 +86,21 @@ class SoftDeleteCollector(Collector):
 
             # delete instances
             for model, instances in self.data.items():
-                query = sql.UpdateQuery(model)
+                query = sql.DeleteQuery(model)
                 pk_list = [obj.pk for obj in instances]
-                query.update_batch(pk_list, {'is_deleted':True},self.using)
-                count = len(pk_list) # TODO:  update_batch method does not return count
-                deleted_counter[model._meta.label] += count
+                count = query.delete_batch(pk_list, self.using)
+                if count:
+                    deleted_counter[model._meta.label] += count
 
                 if not model._meta.auto_created:
                     for obj in instances:
                         signals.post_delete.send(
-                            sender=model, instance=obj, using=self.using
+                            sender=model,
+                            instance=obj,
+                            using=self.using,
+                            origin=self.origin,
                         )
 
-        for instances_for_fieldvalues in self.field_updates.values():
-            for (field, value), instances in instances_for_fieldvalues.items():
-                for obj in instances:
-                    setattr(obj, field.attname, value)
         for model, instances in self.data.items():
             for instance in instances:
                 setattr(instance, model._meta.pk.attname, None)
